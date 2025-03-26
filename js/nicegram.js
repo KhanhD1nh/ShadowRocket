e6c3853042ef8b0cc98ca6de2ea978a7bb5fd67e
KhanhDinh <dgkhanh2003@gmail.com>
2025-03-26 10:57:55 +0700

    Refactor Nicegram module to improve response handling and update naming conventions for modules

--- js/nicegram.js
+++ js/nicegram.js
@@ -1,6 +1,19 @@
-const obj = {
-    "data": {
-        "premiumAccess": true
-    }
-};
-$done({ status: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(obj) });
+var obj;
+
+try {
+    obj = JSON.parse($response.body);
+} catch (e) {
+    obj = {};
+}
+
+if (!obj.data) obj.data = {};
+
+obj.data.premiumAccess = true;
+
+var body = JSON.stringify(obj).replace(/"subscription":\w+/g, '"subscription":true');
+
+$done({
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: body
+});
