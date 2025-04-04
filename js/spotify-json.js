f40956ac68c0f932237cd5f57b69d811335d6be9
Khánh Đinh <101515169+KhanhDinh03@users.noreply.github.com>
2025-04-04 21:16:29 +0700

    Update spotify-json.js

--- js/spotify-json.js
+++ js/spotify-json.js
@@ -2,7 +2,7 @@ let url = $request.url;
 if (url.includes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
 } else {
-    console.log('No processing needed');
+    $notification.post('No processing needed');
 }
 $done({
     url
