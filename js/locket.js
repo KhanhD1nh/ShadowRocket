aa542d03e4c347f9eccef2d7c1428029916881b8
KhanhDinh03 <dgkhanh2003@gmail.com>
2025-06-20 11:56:23 +0700

    Refactor locket.js for improved readability and structure
    - Consolidated header handling by defining a separate 'headers' constant for better clarity.
    - Streamlined the logic for assigning subscriptions and entitlements, enhancing maintainability.
    - Ensured consistent formatting and improved the overall structure of the code for better readability.

--- js/locket.js
+++ js/locket.js
@@ -3,39 +3,49 @@ const mapping = {
   Locket: ["Gold"],
 };
 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
-  obj = JSON.parse($response.body);
+const headers =
+  $request.headers["User-Agent"] || $request.headers["user-agent"];
+const obj = JSON.parse($response.body);
+
 obj.Attention =
   "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-var subscriptions = {
-    is_sandbox: !1,
-    ownership_type: "PURCHASED",
-    billing_issues_detected_at: null,
-    period_type: "normal",
-    expires_date: "2099-12-18T01:04:17Z",
-    grace_period_expires_date: null,
-    unsubscribe_detected_at: null,
-    original_purchase_date: "2003-10-16T13:00:00Z",
-    purchase_date: "2003-10-16T13:00:00Z",
-    store: "app_store",
-  },
-  entitlements = {
-    grace_period_expires_date: null,
-    purchase_date: "2003-10-16T13:00:00Z",
-    product_identifier: "com.khanhdinh.premium.yearly",
-    expires_date: "2099-12-18T01:04:17Z",
-  };
-const match = Object.keys(mapping).find((e) => ua.includes(e));
+
+const subscriptions = {
+  is_sandbox: !1,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2099-12-18T01:04:17Z",
+  grace_period_expires_date: null,
+  unsubscribe_detected_at: null,
+  original_purchase_date: "2003-10-16T13:00:00Z",
+  purchase_date: "2003-10-16T13:00:00Z",
+  store: "app_store",
+};
+
+const entitlements = {
+  grace_period_expires_date: null,
+  purchase_date: "2003-10-16T13:00:00Z",
+  product_identifier: "com.khanhdinh.premium.yearly",
+  expires_date: "2099-12-18T01:04:17Z",
+};
+
+const match = Object.keys(mapping).find((e) => headers.includes(e));
+
 if (match) {
   let [e, s] = mapping[match];
-  s
-    ? ((entitlements.product_identifier = s),
-      (obj.subscriber.subscriptions[s] = subscriptions))
-    : (obj.subscriber.subscriptions["com.khanhdinh.premium.yearly"] =
-        subscriptions),
-    (obj.subscriber.entitlements[e] = entitlements);
-} else
-  (obj.subscriber.subscriptions["com.khanhdinh.premium.yearly"] =
-    subscriptions),
-    (obj.subscriber.entitlements.pro = entitlements);
+
+  if (s) {
+    entitlements.product_identifier = s;
+    obj.subscriber.entitlements[s] = subscriptions;
+  } else {
+    obj.subscriber.subscriptions["com.khanhdinh.premium.yearly"] =
+      subscriptions;
+    obj.subscriber.entitlements[e] = entitlements;
+  }
+} else {
+  obj.subscriber.subscriptions["com.khanhdinh.premium.yearly"] = subscriptions;
+  obj.subscriber.entitlements.pro = entitlements;
+}
+
 $done({ body: JSON.stringify(obj) });
